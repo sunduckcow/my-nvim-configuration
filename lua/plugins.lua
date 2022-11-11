e7e7192ef4bc0bf6948e2ad38df41f5e8bad1ba8
Nikita Sandakov <sunduckcow@gmail.com>
2022-11-11 04:59:11 +0300

    installed packer and basic configuration for it

---
+++ lua/plugins.lua
@@ -0,0 +1,53 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local status, packer = pcall(require, 'packer')
+if not status then
+    return
+end
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'morhetz/gruvbox'
+--  use {
+--    "folke/which-key.nvim",
+--    config = function()
+--      require("which-key").setup {
+--          window = {
+--            border = 'single',
+--             padding = {2, 2, 2, 2},
+--             margin  = {0, 10, 0, 6}
+--          }
+--        -- your configuration comes here
+--        -- or leave it empty to use the default settings
+--        -- refer to the configuration section below
+--      }
+--    end
+--  }
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
