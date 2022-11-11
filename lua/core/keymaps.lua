0171fb5765fae5106f87530c53212fbec328897f
Nikita Sandakov <sunduckcow@gmail.com>
2022-11-11 06:02:55 +0300

    added keymaps for splits and tabs

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,8 +7,19 @@ key.set('i', 'jk', '<ESC>') -- exit insert mode
 key.set('n', '<leader>nh', ':nohl<CR>') -- clear search higlights
 key.set('n', 'x', '"_x') -- no copy to clipboard character deleted with 
 
-key.set('n', '<leader>t', ':echo "Special keymap!!!"<CR>')
+-- key.set('n', '<leader>t', ':echo "Special keymap!!!"<CR>')
 
 -- key.set('n', '<leader>+', '<C-a>')
 -- key.set('n', '<leader>-', '<C-x>') -- useless
 
+-- split window
+key.set('n', '<leader>sv', '<C-w>v')
+key.set('n', '<leader>sh', '<C-w>s')
+key.set('n', '<leader>se', '<C-w>=')
+key.set('n', '<leader>sx', ':close<CR>')
+
+-- tabs
+key.set('n', '<leader>to', ':tabnew<CR>')
+key.set('n', '<leader>tx', ':tabclose<CR>')
+key.set('n', '<leader>tn', ':tabn<CR>')
+key.set('n', '<leader>tp', ':tabp<Cr>')
